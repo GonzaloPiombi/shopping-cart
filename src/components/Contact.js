35ff8bed729556dea77bc2913a8e8e6e5951c44b
Gonzalo <gonzalopiombi@gmail.com>
2022-02-02 15:27:22 -0300

    Create contact page with icons to socials.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,16 @@
 const Contact = () => {
-  return <section>Contact</section>;
+  return (
+    <section>
+      <div className="contact">
+        <a href="https://github.com/GonzaloPiombi">
+          <i className="lab la-github"></i>
+        </a>
+        <i className="lab la-facebook-square"></i>
+        <i className="lab la-instagram"></i>
+        <i className="lab la-twitter"></i>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
