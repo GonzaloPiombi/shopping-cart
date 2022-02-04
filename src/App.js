68f3044bcc1f12d6eb9c708c2a6c3172ede2a5ac
Gonzalo <gonzalopiombi@gmail.com>
2022-02-04 12:16:28 -0300

    Add fetch request to store api.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,34 @@ import Shop from './components/Shop';
 import Contact from './components/Contact';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    let cancel = false;
+    if (!cancel) {
+      return fetchProducts();
+    }
+    return () => {
+      cancel = true;
+    };
+  }, []);
+
+  const fetchProducts = async () => {
+    const data = await fetch(
+      'https://fakestoreapi.com/products/category/electronics'
+    );
+    const items = await data.json();
+    console.log(items);
+    return setProducts(items);
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop" element={<Shop products={products} />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
