0dcb9663fb4b8a1082afb0ab38d8e17c1384ef65
Gonzalo <gonzalopiombi@gmail.com>
2022-02-04 12:17:08 -0300

    Render the products with the data we get back from the api.

---
+++ src/components/ProductCard.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+const ProductCard = (props) => {
+  const [amount, setAmount] = useState(1);
+
+  const handleIncrement = () => {
+    setAmount(amount + 1);
+  };
+
+  const handleDecrement = () => {
+    if (amount === 1) {
+      return;
+    }
+    return setAmount(amount - 1);
+  };
+
+  return (
+    <div className="product-container">
+      <div className="image-container">
+        <img
+          className="grid-image"
+          src={props.product.image}
+          alt={props.product.title}
+        />
+      </div>
+      <div className="details-container">
+        <h2>{props.product.title}</h2>
+        <p>${props.product.price}</p>
+        <div className="counter">
+          <button onClick={handleDecrement}>-</button>
+          <p>{amount}</p>
+          <button onClick={handleIncrement}>+</button>
+        </div>
+        <button className="add-to-cart">Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,15 @@
-const Shop = () => {
-  return <section>Shop</section>;
+import ProductCard from './ProductCard';
+
+const Shop = (props) => {
+  return (
+    <section>
+      <div className="grid-shop">
+        {props.products.map((product) => {
+          return <ProductCard key={product.id} product={product} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Shop;
