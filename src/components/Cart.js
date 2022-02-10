1d6f75736a8ac81215532cabcd65274548765cd0
Gonzalo <gonzalopiombi@gmail.com>
2022-02-10 17:06:32 -0300

    Change layout of jsx.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,27 +3,30 @@ const Cart = (props) => {
     return (
       <div>
         <div className="cart">
-          {props.items.map((item) => {
-            return (
-              <div className="cart-item" key={item.id}>
-                <div className="cart-image">
-                  <img
-                    className="grid-image"
-                    src={item.image}
-                    alt={item.title}
-                  />
+          <h1>My Cart</h1>
+          <div className="container">
+            {props.items.map((item) => {
+              return (
+                <div className="cart-item" key={item.id}>
+                  <div className="cart-image">
+                    <img
+                      className="grid-image"
+                      src={item.image}
+                      alt={item.title}
+                    />
+                  </div>
+                  <div>
+                    <h2>{item.title.slice(0, 15)}...</h2>
+                    <p>${item.price * item.quantity}</p>
+                    <p>{item.quantity}</p>
+                  </div>
                 </div>
-                <div>
-                  <h1>{item.title.slice(0, 15)}...</h1>
-                  <p>${item.price * item.quantity}</p>
-                  <p>{item.quantity}</p>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
           <div className="cart-buttons">
             <h1>
-              Total:
+              Total: $
               {props.items.reduce((acc, curr) => {
                 return (acc += curr.price * curr.quantity);
               }, 0)}
